434a1c100629a7f0983be6392f3b186034054ab9
ijhwlee@gmail.com <hwlee@inje.ac.kr>
2022-12-02 22:42:36 +0900

    add auto rotate button

--- y2022/PageNavigate/PageNavigate/HelloXamlPage.xaml.cs
+++ y2022/PageNavigate/PageNavigate/HelloXamlPage.xaml.cs
@@ -2,14 +2,38 @@ namespace PageNavigate;
 
 public partial class HelloXamlPage : ContentPage
 {
+	IDispatcherTimer timer;
+	bool timerOn = false;
 	public HelloXamlPage()
 	{
 		InitializeComponent();
 		LabelHello.TranslationY = 300;
+		SetTimer();
 	}
 
-	private void ToggleRoate_Clicked(object sender, EventArgs e)
+	private void ToggleRotate_Clicked(object sender, EventArgs e)
 	{
+		timerOn = !timerOn;
+		if (timerOn)
+		{
+			timer.Start();
+			ToggleRotate.BackgroundColor = Colors.Red;
+		}
+		else
+		{
+			timer.Stop();
+      ToggleRotate.BackgroundColor = Colors.Blue;
+    }
+  }
+	private void SetTimer()
+	{
+    timer = Dispatcher.CreateTimer();
+    timer.Interval = TimeSpan.FromMilliseconds(100);
+    timer.Tick += Timer_Tick;
+  }
+
+  private void Timer_Tick(object sender, EventArgs e)
+  {
 		LabelHello.Rotation = LabelHello.Rotation + 10;
-	}
+  }
 }
