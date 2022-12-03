8d497d843148b29359cfa2e8e6c697f8d7184cd7
ijhwlee <hwlee@inje.ac.kr>
2022-12-03 10:19:56 +0900

    chnage solution directory name to Fundamentals

---
+++ y2022/Fundamentals/PageNavigate/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PageNavigate;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ y2022/Fundamentals/PageNavigate/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace PageNavigate;
+
+public partial class MainPage : ContentPage
+{
+	HelloXamlPage helloPage = new HelloXamlPage();
+	public MainPage()
+	{
+		InitializeComponent();
+		Button button = new Button
+		{
+			Text = "Navigate!",
+			HorizontalOptions = LayoutOptions.Center,
+			VerticalOptions = LayoutOptions.Center,
+		};
+		button.Clicked += async (sender, args) =>
+		{
+			await Navigation.PushAsync(helloPage);
+		};
+		Content = button;
+	}
+
+}
+
