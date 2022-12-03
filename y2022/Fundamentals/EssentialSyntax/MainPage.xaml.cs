983c170e85690e5f1ad06a5eac8f1b71b6675a19
ijhwlee <hwlee@inje.ac.kr>
2022-12-03 10:34:02 +0900

    ass essential syntax

---
+++ y2022/Fundamentals/EssentialSyntax/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EssentialSyntax
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ y2022/Fundamentals/EssentialSyntax/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EssentialSyntax
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
