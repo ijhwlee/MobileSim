3d4c7d9bc7d360ecc6116957e11c5b5f52e5d2b4
ijhwlee <hwlee@inje.ac.kr>
2022-12-13 00:34:33 +0900

    add DI test

---
+++ y2022/MAUIProjects/MauiDITest/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiDITest
+{
+  public partial class MainPage : ContentPage
+  {
+    //private readonly IPlatformDiService _platformDiService;
+    int count = 0;
+
+    public MainPage(MainPageViewModel mainPageViewModel)
+    {
+      InitializeComponent();
+      this.BindingContext = mainPageViewModel;
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+      count++;
+
+      if (count == 1)
+        CounterBtn.Text = $"Clicked {count} time";
+      else
+        CounterBtn.Text = $"Clicked {count} times";
+
+      SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+  }
+}
---
+++ y2022/MAUIProjects/MauiDITest/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiDITest;
+
+public class MainPageViewModel : INotifyPropertyChanged
+{
+  private readonly IPlatformDiService _platformDiService;
+  private string sayYourPlatformNameValue = "Click the 'Reveal platform' button";
+  private Command _sayYourPlatformNameCommand;
+
+  public event PropertyChangedEventHandler PropertyChanged;
+
+  public MainPageViewModel(IPlatformDiService platformDiService) 
+  { 
+    _platformDiService = platformDiService;
+  }
+
+  public void OnPropertyChanged(string propertyName)
+  {
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+  }
+  public string SayYourPlatformNameValue
+  {
+    get => sayYourPlatformNameValue;
+    set
+    {
+      sayYourPlatformNameValue = value;
+      OnPropertyChanged(nameof(this.SayYourPlatformNameValue));
+    }
+  }
+  public Command SayYourPlatformNameCommand => _sayYourPlatformNameCommand ??=
+    new Command( () => { this.SayYourPlatformNameValue = _platformDiService.SayYourPlatformName(); });
+}
